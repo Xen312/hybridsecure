fdffca1cd89f20c7bd6c6e30bf1d1423377a40ba
Xenocide31 <ahongsangbamc@gmail.com>
2025-12-16 22:23:21 +0530

    work this time

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import { google } from "googleapis";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(__dirname));
+
 
 dotenv.config();
 
